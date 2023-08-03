22a0078e34a64c9bb4f584cdfa7eea40de079472
SAHA-YJB <pkomom123@naver.com>
2023-08-03 12:59:01 +0900

    style: 스타일드 컴포넌트 호환문제

---
+++ components/SignUp/SingUpUi.tsx
@@ -0,0 +1,69 @@
+import { Checkbox, SignEmailBox, SignUpCheckBox, SignUpCheckBoxLayout, SignUpEmailBtn, SignUpEmailInput, SignUpNickNameBox, SignUpNickNameInput, SignUpPasswordBox, SignUpSection, SignUpSocialSignUpBox, SingUpPasswordInput, SingUpPasswordValidInput, StyledLabel} from '@/styles/signup'
+import React from 'react'
+
+const SignUpUi = () => {
+  return (
+    <SignUpSection>
+      <h1>
+          HAPOOM
+      </h1>
+      <h2>
+          회원가입
+      </h2>
+      
+      <SignUpSocialSignUpBox>
+        <p>sns계정으로 간편 로그인/회원가입</p>
+        <div>구글</div>
+        <div>카카오</div>
+        <div>인스타</div>
+      </SignUpSocialSignUpBox>
+      
+      <form name='register'>
+        <SignEmailBox>
+          <p>이메일</p>
+          <SignUpEmailInput />
+          <SignUpEmailBtn>이메일 인증하기</SignUpEmailBtn>
+        </SignEmailBox>
+
+        <SignUpPasswordBox>
+          <p>비밀번호</p>
+          <p>영문, 숫자를 포함한 8자이상의 비밀번호를 입력해주세요</p>
+          <SingUpPasswordInput />
+          <p>비밀번호 확인</p>
+          <SingUpPasswordValidInput />
+        </SignUpPasswordBox>
+
+        <SignUpNickNameBox>
+          <p>닉네임</p>
+          <p>다른 유저와 겹치지 않도록 입력해주세요(2~15자)</p>
+          <SignUpNickNameInput />
+        </SignUpNickNameBox>
+
+        <SignUpCheckBoxLayout>
+          <p>약관동의</p>
+        <SignUpCheckBox>
+
+          <Checkbox/>
+            <label htmlFor="check-all"></label>
+          <StyledLabel>전체동의</StyledLabel>
+
+          <Checkbox/>
+            <label htmlFor="check-terms"></label>
+            <StyledLabel>이용약관 (필수)</StyledLabel>
+
+          <Checkbox/>
+            <label htmlFor="check-personalInfo"></label>
+            <StyledLabel>개인정보 수집/이용 동의 (필수)</StyledLabel>
+        
+          <Checkbox/>
+            <label htmlFor="check-newsletter"></label>
+            <StyledLabel>개인정보 마케팅 활용 동의 (선택)</StyledLabel>
+          </SignUpCheckBox>
+        </SignUpCheckBoxLayout>
+
+      </form>
+    </SignUpSection>
+  )
+}
+
+export default SignUpUi
---
+++ styles/signUp.ts
@@ -0,0 +1,29 @@
+import styled from 'styled-components';
+
+export const SignUpSection = styled.section`
+  max-width: 1440px; 
+  width: 100%;
+  margin: 0 auto;
+  display: flex;
+  justify-content: center;
+  /* align-items: center; */
+`
+
+export const SignUpSocialSignUpBox = styled.div``
+
+export const  SignEmailBox = styled.div``
+export const SignUpEmailInput = styled.input``
+export const  SignUpEmailBtn= styled.button``
+
+export const SignUpPasswordBox = styled.div``
+export const SingUpPasswordInput = styled.input``
+export const SingUpPasswordValidInput = styled.input``
+
+export const SignUpNickNameBox = styled.div``
+export const SignUpNickNameInput = styled.input``
+
+export const SignUpCheckBoxLayout = styled.div``
+export const SignUpCheckBox = styled.div``
+
+export const Checkbox = styled.input``
+export const StyledLabel = styled.div``
