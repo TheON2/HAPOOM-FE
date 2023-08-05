2de62463dcf32e9963c6fd6a4ba2366282c17a0b
Haegnim <haegnim@gmail.com>
2023-08-05 17:12:29 +0900

    Create : Footer

---
+++ styles/footer.ts
@@ -0,0 +1,31 @@
+import styled from 'styled-components';
+
+export const FooterLayout = styled.footer`
+  width: 100%;
+  height: 30vh;
+  background-color: gray;
+`;
+
+export const FooterBox = styled.div`
+  max-width: 1440px;
+  width: 100%;
+  padding: 30px 20px 100px;
+  display: flex;
+  gap: 24px;
+`;
+
+export const LogoBox = styled.div`
+  width: 30%;
+  height: 50px;
+  background-color: #000;
+  position: relative;
+  img {
+    object-fit: contain;
+  }
+`;
+export const FooterContent = styled.div`
+  width: 70%;
+  color: #fff;
+  font-size: 0.8rem;
+  line-height: 1.4rem;
+`;
