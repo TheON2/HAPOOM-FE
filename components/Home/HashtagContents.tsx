f53b6fdcfe4e8968f60317ce475fe589f1206306
TheON2 <sniperad@naver.com>
2023-08-07 13:54:16 +0900

    Fix

--- components/Home/HashtagContents.tsx
+++ components/Home/HashtagContents.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import styled from 'styled-components';
-import { SliderImage } from '@/public/data';
 import ImageContent from '@/components/Home/ImageContent';
 import {
   SectionTitle,
