bf70f3f8e8988ea10374a15e224e7e1fd6f4d05a
Haegnim <haegnim@gmail.com>
2023-08-07 14:05:42 +0900

    Create : setting page

---
+++ components/Setting/UserProfileImageUpdate.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import styled from 'styled-components';
+
+// const ImageBox = styled.div`
+// width: ;
+// `
+
+const UserProfileImageUpdate = () => {
+  return <div>UserProfileImageUpdate</div>;
+};
+
+export default UserProfileImageUpdate;
