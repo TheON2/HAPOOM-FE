5009c36ebb4e7a6528ba8a41483007c5d1d4a9b4
SAHA-YJB <pkomom123@naver.com>
2023-08-07 16:46:46 +0900

    feat: features/signip signin logic 90%

--- components/SignIn/SignInUi.tsx
+++ components/SignIn/SignInUi.tsx
@@ -11,6 +11,14 @@ import {
 } from '@/styles/signIn';
 import React, { useState } from 'react';
 import SocialLogin from './SocialLogIn';
+import { useRouter } from 'next/router';
+import { useDispatch } from 'react-redux';
+import { useMutation } from 'react-query';
+import { NextRouter } from 'next/router';
+import { FormEvent } from 'react';
+import { userLoginTest } from '@/api/test/test_user';
+import { LOGIN_USER } from '@/redux/reducers/userSlice';
+import { userLogin } from '@/api/user';
 
 interface SignIn {
   email: string;
@@ -18,11 +26,33 @@ interface SignIn {
 }
 
 const SignInUi = () => {
+  const dispatch: any = useDispatch();
+  const router: NextRouter = useRouter();
   const [signInState, setSignInState] = useState<SignIn>({
     email: '',
     password: '',
   });
+  const [error, setError] = useState<SignIn>({
+    email: '',
+    password: '',
+  });
 
+  const signInMutation = useMutation(userLogin, {
+    onSuccess: (data) => {
+      dispatch(LOGIN_USER(data));
+      console.log('로그인 성공');
+      router.push('/');
+    },
+    onError: (error: any) => {
+      if (error.response && error.response.data) {
+        setError((prev) => ({ ...prev, password: error.response.data }));
+      }
+    },
+  });
+
+  const moveSignUpBtn = () => {
+    router.push('/auth/SignUp');
+  };
   const validateEmail = (email: string): boolean => {
     const emailRegex = /^[\w-]+(\.[\w-]+)*@([\w-]+\.)+[a-zA-Z]{2,7}$/;
     return emailRegex.test(email);
@@ -39,9 +69,35 @@ const SignInUi = () => {
     }));
   };
 
-  const handleLogin = () => {
-    console.log('로그인 처리 로직 작성');
+  const handleLogin = (e: FormEvent) => {
+    e.preventDefault();
+    let errors: any = {};
+
+    if (!signInState.email) {
+      errors.email = '이메일 주소를 입력해주세요.';
+    } else if (!validateEmail(signInState.email)) {
+      errors.email = '이메일 형식이 아닙니다';
+    }
+
+    if (!signInState.password) {
+      errors.password = '비밀번호를 입력해주세요.';
+    } else if (!validatePassword(signInState.password)) {
+      errors.password = '비밀번호를 확인해주세요';
+    }
+
+    if (Object.keys(errors).length > 0) {
+      setError(errors);
+      return;
+    } else {
+      setError({ email: '', password: '' });
+    }
+    const sendData = {
+      email: signInState.email,
+      password: signInState.password,
+    };
+    signInMutation.mutate(sendData);
   };
+
   return (
     <SignInSection>
       <SignInContainer>
@@ -55,6 +111,7 @@ const SignInUi = () => {
             placeholder="이메일을 입력해 주세요"
             onChange={handleInputChange}
           />
+          {error.email && <p style={{ color: 'red' }}>{error.email}</p>}
         </StyledInputBox>
 
         <StyledInputBox>
@@ -65,20 +122,13 @@ const SignInUi = () => {
             placeholder="비밀번호를 입력해 주세요"
             onChange={handleInputChange}
           />
+          {error.password && <p style={{ color: 'red' }}>{error.password}</p>}
         </StyledInputBox>
 
-        <SignInBtn
-          onClick={(e: React.MouseEvent) => {
-            e.preventDefault();
-            handleLogin();
-            alert('준비');
-          }}
-        >
-          로그인
-        </SignInBtn>
+        <SignInBtn onClick={handleLogin}>로그인</SignInBtn>
         <PwdSignUpSettinPageLink>
-          <p>비밀번호 재설정</p>
-          <p>회원가입</p>
+          <p onClick={() => alert('준비중입니다.')}>비밀번호 재설정</p>
+          <p onClick={moveSignUpBtn}>회원가입</p>
         </PwdSignUpSettinPageLink>
         <TextParagraphSns>sns계정으로 간편로그인/회원가입</TextParagraphSns>
 
--- components/SignIn/SocialLogIn.tsx
+++ components/SignIn/SocialLogIn.tsx
@@ -10,9 +10,30 @@ const SocialLogin = () => {
   };
   return (
     <SignUpSocialSignUpBox>
-      <SocialBoxImg onClick={onSocialLogIn} src={googleLogo} alt="구글로고" />
-      <SocialBoxImg onClick={onSocialLogIn} src={kakaoLogo} alt="카카오로고" />
-      <SocialBoxImg onClick={onSocialLogIn} src={instaLogo} alt="인스타로고" />
+      <SocialBoxImg
+        width={84}
+        height={84}
+        onClick={onSocialLogIn}
+        src={googleLogo}
+        alt="구글로고"
+        quality={80}
+      />
+      <SocialBoxImg
+        width={84}
+        height={84}
+        onClick={onSocialLogIn}
+        src={kakaoLogo}
+        alt="카카오로고"
+        quality={80}
+      />
+      <SocialBoxImg
+        width={84}
+        height={84}
+        onClick={onSocialLogIn}
+        src={instaLogo}
+        alt="인스타로고"
+        quality={80}
+      />
     </SignUpSocialSignUpBox>
   );
 };
