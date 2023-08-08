d375ef386359c77787277c910f3e0650dd97275f
SAHA-YJB <pkomom123@naver.com>
2023-08-08 14:13:20 +0900

    style: style figma version update

--- components/SignIn/SocialLogIn.tsx
+++ components/SignIn/SocialLogIn.tsx
@@ -2,7 +2,8 @@ import React from 'react';
 import googleLogo from '../../public/googleLogo.png';
 import kakaoLogo from '../../public/kakaoLogo.png';
 import instaLogo from '../../public/pngegg.png';
-import { SignUpSocialSignUpBox, SocialBoxImg } from '@/styles/signIn';
+import { SignUpSocialSignUpBox } from '@/styles/signIn';
+import Image from 'next/image';
 
 const SocialLogin = () => {
   const onSocialLogIn = () => {
@@ -10,29 +11,32 @@ const SocialLogin = () => {
   };
   return (
     <SignUpSocialSignUpBox>
-      <SocialBoxImg
-        width={84}
-        height={84}
+      <Image
+        width={64}
+        height={64}
         onClick={onSocialLogIn}
         src={googleLogo}
         alt="구글로고"
         quality={80}
+        style={{ borderRadius: '100%', cursor: 'pointer' }}
       />
-      <SocialBoxImg
-        width={84}
-        height={84}
+      <Image
+        width={64}
+        height={64}
         onClick={onSocialLogIn}
         src={kakaoLogo}
         alt="카카오로고"
         quality={80}
+        style={{ borderRadius: '100%', cursor: 'pointer' }}
       />
-      <SocialBoxImg
-        width={84}
-        height={84}
+      <Image
+        width={64}
+        height={64}
         onClick={onSocialLogIn}
         src={instaLogo}
         alt="인스타로고"
         quality={80}
+        style={{ borderRadius: '100%', cursor: 'pointer' }}
       />
     </SignUpSocialSignUpBox>
   );
