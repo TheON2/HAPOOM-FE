ecff1d189996b7f6aeb05fcf6edb372c498da615
Haegnim <haegnim@gmail.com>
2023-08-15 13:26:10 +0900

    Design : search page css

--- components/common/SelectUI.tsx
+++ components/common/SelectUI.tsx
@@ -24,27 +24,29 @@ export const Selecter = ({ selectOption, setOption }: selectProps) => {
   };
 
   return (
-    <>
+    <SelectBox $show={isShow}>
       <SelectBarButton type="button" onClick={SelectClick}>
         {selectText}
         <RightArrow />
       </SelectBarButton>
       {isShow && (
         <SelectList>
-          {selectOption.map((option, idx) => {
-            return (
-              <SelectItem
-                key={idx}
-                value={option.value}
-                onClick={() => OptionClick(option.value, option.text)}
-              >
-                {option.text}
-              </SelectItem>
-            );
-          })}
+          {selectOption
+            .filter((option) => option.text !== selectText)
+            .map((option, idx) => {
+              return (
+                <SelectItem
+                  key={idx}
+                  value={option.value}
+                  onClick={() => OptionClick(option.value, option.text)}
+                >
+                  {option.text}
+                </SelectItem>
+              );
+            })}
         </SelectList>
       )}
-    </>
+    </SelectBox>
   );
 };
 // 이게 꼭 필요한 것인가?
@@ -52,33 +54,60 @@ export const Selecter = ({ selectOption, setOption }: selectProps) => {
 //     return <ItemNonHidden>{children}</ItemNonHidden>;
 // };
 const SelectBarButton = styled.button`
-  width: 250px;
-  height: 40px;
-  padding: 0px 20px;
+  /* width: 250px; */
+  width: 100%;
+  height: 36px;
+  padding: 0px 16px;
+  background: none;
+  border: none;
+  border-radius: 20px;
   display: flex;
   justify-content: space-between;
   align-items: center;
+  /* gap: 16px; */
+  color: #fff;
+  font-size: 12px;
+  font-weight: 700;
+  svg {
+    transform: scale(0.8) rotate(90deg);
+    path {
+      fill: #fff;
+    }
+  }
 `;
 const SelectList = styled.ul`
-  width: 250px;
-  background-color: #efefef;
-  position: absolute;
-  top: 98px;
+  width: 80px;
+  /* background-color: #5aabf6; */
+  /* position: absolute;
+  top: 98px; */
 `;
 const SelectItem = styled.li`
   width: 100%;
-  height: 40px;
-  padding: 10px;
-  font-size: 14px;
-  font-weight: 600;
+  height: 36px;
+  padding: 0px 16px;
+  color: #fff;
+  font-size: 12px;
+  font-weight: 700;
   display: flex;
   align-items: center;
-  color: #000;
   cursor: pointer;
   &:hover {
     background-color: gray;
   }
 `;
-const SelectBox = styled.div`
-  height: 40px;
+
+type Props = {
+  $show: boolean;
+};
+
+const SelectBox = styled.div<Props>`
+  width: 80px;
+  height: ${(props) => (props.$show ? 'calc(36px * 3)' : '36px')};
+  background-color: #5aabf6;
+  border-radius: 20px;
+  position: absolute;
+  z-index: 14;
+  /* top: 4px; */
+  /* left: 12px; */
+  /* transform: translateY(-50%); */
 `;
--- pages/search/index.tsx
+++ pages/search/index.tsx
@@ -5,34 +5,80 @@ import styled from 'styled-components';
 import { Selecter } from '@/components/common/SelectUI';
 import useInput from '@/hooks/useInput';
 import Button from '@/components/common/Button';
+import IconButton from '@/components/common/IconButton';
 import ImageContent from '@/components/Home/ImageContent';
 import Footer from '@/components/common/Footer';
 import { ImageContentsContainer } from '@/styles/imageContainer';
 import { hashtagContentsImages } from '@/public/data';
-const Main = styled.main`
+import { Cloud } from '@/components/common/SVG';
+
+const SearchLayout = styled.main`
   width: 100%;
-  background-color: gray;
+  padding: 20px 24px;
   .search-button {
-    width: 200px;
+    width: 32px;
+    height: 32px;
+    background-color: #5bb0ff;
+    border-radius: 18px;
+    transform: translateX(0);
+    svg {
+      fill: #fff;
+    }
+  }
+  .keyword {
+    font-size: 14px;
+    font-weight: bold;
+    text-align: center;
+    padding: 0px 0 20px;
+    margin: 0px 0 20px;
+    &::after {
+      content: '';
+      display: block;
+      width: 120px;
+      height: 2px;
+      margin: 0 auto;
+      transform: translateY(20px);
+      background-color: #dcd8d8;
+    }
+    .highligth {
+      color: #0084ff;
+    }
   }
 `;
 
 const SearchForm = styled.form`
   display: flex;
-  flex-direction: column;
+  width: 100%;
+  height: 46px;
+  padding: 12px 5px;
+  margin: 0 auto 36px;
+  /* flex-direction: column; */
+  justify-content: space-between;
   align-items: center;
+  gap: 4px;
+  background-color: #fff;
+  border: 1px solid #5bb0ff;
+  border-radius: 30px;
+  box-shadow: 0 5px 8px #3a51661f;
+  position: relative;
 `;
 
 const SearchInput = styled.input`
-  width: 80%;
-  height: 44px;
-  padding: 20px 24px;
+  width: 60%;
+  height: 36px;
+  padding: 8px 16px;
+  border-radius: 22px;
+  border: none;
+  font-size: 12px;
+  &:focus {
+    outline: none;
+  }
 `;
 
 const SelectBox = styled.div`
-  width: 80%;
-  height: 100px;
-  background-color: gray;
+  width: 80px;
+  height: 36px;
+  position: relative;
 `;
 
 const selectOption = [
@@ -43,6 +89,7 @@ const selectOption = [
 const Search = () => {
   const [search, searchHandler, setSearch] = useInput('');
   const [option, setOption] = useState<string>();
+
   const onSubmitSearchHandler = (e: FormEvent) => {
     e.preventDefault();
     alert(search + option);
@@ -51,20 +98,33 @@ const Search = () => {
   return (
     <>
       <Header />
-      <Main>
+      <SearchLayout>
+        <p className="keyword">
+          {search === '' ? (
+            <>
+              오늘은 어떤 <span className="highligth">하늘</span>을
+              검색해볼까요?
+            </>
+          ) : (
+            <>
+              <span className="highligth">{search}</span>에 대한 결과입니다.
+            </>
+          )}
+        </p>
         <SearchForm onSubmit={onSubmitSearchHandler}>
-          <Selecter selectOption={selectOption} setOption={setOption} />
+          <SelectBox>
+            <Selecter selectOption={selectOption} setOption={setOption} />
+          </SelectBox>
           <SearchInput
             type="text"
             value={search}
             name="search"
             onChange={searchHandler}
           />
-          <Button type="submit" className="search-button">
-            검색
-          </Button>
+          <IconButton type="submit" className="search-button">
+            <Cloud />
+          </IconButton>
         </SearchForm>
-        <SelectBox />
         <ImageContentsContainer>
           {hashtagContentsImages.map((result, idx) => {
             return (
@@ -77,9 +137,8 @@ const Search = () => {
             );
           })}
         </ImageContentsContainer>
-      </Main>
+      </SearchLayout>
       <Footer />
-      <MobileBottomNav />
     </>
   );
 };
