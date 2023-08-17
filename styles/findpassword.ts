2f4f7c85ce4fe2550f71c1f993ddcb6943f5b520
Haegnim <haegnim@gmail.com>
2023-08-17 17:08:59 +0900

    Fix : edit styled component props naming

--- styles/findpassword.ts
+++ styles/findpassword.ts
@@ -1,17 +1,17 @@
 import Image from 'next/image';
 import styled from 'styled-components';
 
-const inputBtnBoxWidth = "100%";
-const inputBtnHeight = "44px";
+const inputBtnBoxWidth = '100%';
+const inputBtnHeight = '44px';
 const theme = {
-  textColor: "#000",
-  fontFamily: "Inter",
-  primaryColor: "#0084FF",
-  inputBtnBoxWidth: "100%"
+  textColor: '#000',
+  fontFamily: 'Inter',
+  primaryColor: '#0084FF',
+  inputBtnBoxWidth: '100%',
 };
 interface Props {
-  marginBottom?: string;
-  marginTop?: string;
+  $marginBottom?: string;
+  $marginTop?: string;
   color?: string;
   width?: string;
   borderColor?: string;
@@ -32,7 +32,7 @@ export const FindPwdSection = styled.section`
   }
 `;
 export const MainHeadText = styled.h1<Props>`
-  color: #0084FF;
+  color: #0084ff;
   text-align: center;
   font-size: 48px;
   font-weight: 900;
@@ -42,8 +42,8 @@ export const SubHeadText = styled.h2<Props>`
   color: ${({ color }) => color};
   font-size: 13px;
   font-weight: 700;
-  margin-bottom: ${({ marginBottom }) => marginBottom || '0'};
-  margin-top: ${({ marginTop }) => marginTop || '0'};
+  margin-bottom: ${({ $marginBottom }) => $marginBottom || '0'};
+  margin-top: ${({ $marginTop }) => $marginTop || '0'};
 `;
 export const TextParagraph = styled.p`
   color: ${theme.textColor};
@@ -55,17 +55,17 @@ export const TextParagraphInfo = styled(TextParagraph)<Props>`
   font-size: 12px;
   font-weight: 700;
   margin-top: 10px;
-  margin-bottom: ${({ marginBottom }) => marginBottom || '0'};
+  margin-bottom: ${({ $marginBottom }) => $marginBottom || '0'};
 `;
 export const TextParagraphSns = styled(TextParagraph)`
   margin-top: 4px;
-  color: #B1B1B1;
+  color: #b1b1b1;
   text-align: center;
   font-size: 10px;
   font-weight: 400;
 `;
 export const TextParagrapValidate = styled(TextParagraph)`
-  color: #828C8C;
+  color: #828c8c;
   font-size: 11px;
   font-weight: 400;
   margin-bottom: 10px;
@@ -89,17 +89,16 @@ export const StyledInputBox = styled.div`
   /* max-width: 312px; */
   display: flex;
   flex-direction: column;
-  margin-bottom:12px;
-  
+  margin-bottom: 12px;
 `;
 export const StyledInput = styled.input<Props>`
-  width: 100%; 
+  width: 100%;
   height: 36px;
   outline: none;
   padding-left: 28px;
-  border: 1px solid ${({borderColor}) => borderColor};
+  border: 1px solid ${({ borderColor }) => borderColor};
   border-radius: 3px;
-  &::placeholder{
+  &::placeholder {
     font-size: 12px;
   }
 `;
@@ -109,7 +108,7 @@ export const FindPwdBtn = styled.button`
   border-radius: 3px;
   background: ${theme.primaryColor};
   border: 1px solid ${theme.primaryColor};
-  color: #FFF;
+  color: #fff;
   text-align: center;
   font-size: 16px;
   font-weight: 700;
@@ -119,22 +118,23 @@ export const SnsLine = styled.div`
   stroke-width: 1px;
   width: 100%;
   height: 0px;
-  border: 1px solid #B3B3B3;
+  border: 1px solid #b3b3b3;
   margin: 20px 0 22px 0;
-`
+`;
 export const Line = styled.div<Props>`
   /* width: 312px; */
-  width: ${(props) => props.width ? props.width : '312px' };
-  border: 1px solid ${(props) => props.borderColor ? props.borderColor : '#0084FF'};
+  width: ${(props) => (props.width ? props.width : '312px')};
+  border: 1px solid
+    ${(props) => (props.borderColor ? props.borderColor : '#0084FF')};
   margin-bottom: 16px;
-`
+`;
 export const TextErrorParagraph = styled.p`
   max-width: 227px;
-  height: 28px; 
+  height: 28px;
   color: red;
   font-family: ${theme.fontFamily};
   font-size: 10px;
   font-weight: 400;
   line-height: 2.5;
   margin-left: 4px;
-  `;
+`;
