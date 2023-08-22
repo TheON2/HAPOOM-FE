8af7d3f763bb19e2b550c889b748121b2b39cbfa
SAHA-YJB <pkomom123@naver.com>
2023-08-22 21:37:54 +0900

    feat: components destructure work

--- components/SignUp/SignUpCheck.tsx
+++ components/SignUp/SignUpCheck.tsx
@@ -1,11 +1,5 @@
-import React, { ChangeEvent, useCallback, useState } from 'react';
+import React from 'react';
 import {
-  SignUpSection,
-  MainHeadText,
-  SubHeadText,
-  SignUpBtn,
-  StyledInputBox,
-  StyledInput,
   Checkbox,
   SignUpCheckBox,
   SignUpCheckBoxLayout,
@@ -14,14 +8,87 @@ import {
   Line,
   TextErrorParagraph,
   StyledLabelEssential,
-  TextParagraphSns,
-  SnsLine,
-  TextParagraphInfo,
-  TextParagrapValidate,
 } from '@/styles/signUp';
-import { useMutation } from 'react-query';
-import { addUser } from '@/api/user';
-import { NextRouter, useRouter } from 'next/router';
-import MobileBottomNav from '../common/MobileBottomNav';
-import SocialLogin from '../SignIn/SocialLogIn';
-import { SecretEye } from '../common/SVG';
+import { CheckBoxInterface } from './SingUpUi';
+
+interface SignUpCheckProps {
+  checkboxes: CheckBoxInterface;
+  handleCheckboxChange: (
+    e: React.ChangeEvent<HTMLInputElement & { name: keyof CheckBoxInterface }>
+  ) => void;
+  checkboxErrorMessage: string;
+}
+
+const SignUpCheck: React.FC<SignUpCheckProps> = ({
+  checkboxes,
+  handleCheckboxChange,
+  checkboxErrorMessage,
+}) => {
+  return (
+    <>
+      <SignUpCheckBoxLayout>
+        <SignUpCheckBox>
+          <Checkbox
+            id="checkAll"
+            type="checkbox"
+            name="checkAll"
+            checked={checkboxes.checkAll}
+            onChange={handleCheckboxChange}
+          />
+          <label htmlFor="checkAll"></label>
+          <StyledLabelAll>전체동의</StyledLabelAll>
+          <StyledLabel>선택항목에 대한 동의 포함</StyledLabel>
+        </SignUpCheckBox>
+        <Line></Line>
+
+        <SignUpCheckBox>
+          <Checkbox
+            id="check-terms"
+            type="checkbox"
+            name="checkTerms"
+            checked={checkboxes.checkTerms}
+            onChange={handleCheckboxChange}
+          />
+          <label htmlFor="check-terms"></label>
+          <StyledLabelEssential>이용약관 (필수)</StyledLabelEssential>
+        </SignUpCheckBox>
+
+        <SignUpCheckBox>
+          <Checkbox
+            id="check-personalInfo"
+            type="checkbox"
+            name="checkPersonalInfo"
+            checked={checkboxes.checkPersonalInfo}
+            onChange={handleCheckboxChange}
+          />
+          <label htmlFor="check-personalInfo"></label>
+          <StyledLabelEssential>
+            개인정보 수집/이용 동의 (필수)
+          </StyledLabelEssential>
+        </SignUpCheckBox>
+
+        <SignUpCheckBox>
+          <Checkbox
+            id="check-newsletter"
+            type="checkbox"
+            name="checkNewsletter"
+            checked={checkboxes.checkNewsletter}
+            onChange={handleCheckboxChange}
+          />
+          <label htmlFor="check-newsletter"></label>
+          <StyledLabelEssential>
+            개인정보 마케팅 활용 동의 (필수)
+          </StyledLabelEssential>
+        </SignUpCheckBox>
+      </SignUpCheckBoxLayout>
+
+      {checkboxErrorMessage && (
+        <TextErrorParagraph style={{ marginTop: '10px' }}>
+          {checkboxErrorMessage}
+        </TextErrorParagraph>
+      )}
+    </>
+  );
+};
+
+export default React.memo(SignUpCheck);
--- components/SignUp/SignUpPwd.tsx
+++ components/SignUp/SignUpPwd.tsx
@@ -1,27 +1,99 @@
-import React, { ChangeEvent, useCallback, useState } from 'react';
+import React, { useCallback, useState } from 'react';
 import {
-  SignUpSection,
-  MainHeadText,
-  SubHeadText,
-  SignUpBtn,
   StyledInputBox,
   StyledInput,
-  Checkbox,
-  SignUpCheckBox,
-  SignUpCheckBoxLayout,
-  StyledLabel,
-  StyledLabelAll,
-  Line,
   TextErrorParagraph,
-  StyledLabelEssential,
-  TextParagraphSns,
-  SnsLine,
   TextParagraphInfo,
   TextParagrapValidate,
 } from '@/styles/signUp';
-import { useMutation } from 'react-query';
-import { addUser } from '@/api/user';
-import { NextRouter, useRouter } from 'next/router';
-import MobileBottomNav from '../common/MobileBottomNav';
-import SocialLogin from '../SignIn/SocialLogIn';
 import { SecretEye } from '../common/SVG';
+
+interface Props {
+  signUpState: {
+    password: string;
+    passwordConfirm: string;
+  };
+  handleInputChange: (
+    e: React.ChangeEvent<HTMLInputElement & { name: string }>
+  ) => void;
+  error: {
+    password: string;
+    passwordConfirm: string;
+  };
+}
+
+const SignUpPwd: React.FC<Props> = ({
+  signUpState,
+  handleInputChange,
+  error,
+}) => {
+  const [passwordInputType, setPasswordInputType] = useState<
+    'password' | 'text'
+  >('password');
+  const [passwordConfirmInputType, setPasswordConfirmInputType] = useState<
+    'password' | 'text'
+  >('password');
+
+  const togglePasswordVisibility = useCallback(() => {
+    setPasswordInputType(
+      passwordInputType === 'password' ? 'text' : 'password'
+    );
+  }, [passwordInputType]);
+
+  const togglePasswordConfirmVisibility = useCallback(() => {
+    setPasswordConfirmInputType(
+      passwordConfirmInputType === 'password' ? 'text' : 'password'
+    );
+  }, [passwordConfirmInputType]);
+  return (
+    <StyledInputBox>
+      <TextParagraphInfo $marginBottom="7px">비밀번호</TextParagraphInfo>
+      <TextParagrapValidate>
+        영문, 숫자를 포함한 8자 이상의 비밀번호를 입력해주세요.
+      </TextParagrapValidate>
+      <StyledInput
+        type={passwordInputType}
+        name="password"
+        value={signUpState.password}
+        placeholder="비밀번호"
+        onChange={handleInputChange}
+      />
+      <SecretEye
+        onClick={togglePasswordVisibility}
+        style={{
+          position: 'absolute',
+          right: '10px',
+          top: '50%',
+          transform: 'translateY(-50%)',
+          cursor: 'pointer',
+        }}
+      />
+      {error.password && (
+        <TextErrorParagraph>{error.password}</TextErrorParagraph>
+      )}
+      <TextParagraphInfo>비밀번호 확인</TextParagraphInfo>
+      <StyledInput
+        type={passwordConfirmInputType}
+        name="passwordConfirm"
+        value={signUpState.passwordConfirm}
+        placeholder="비밀번호 확인"
+        onChange={handleInputChange}
+      />
+      <SecretEye
+        onClick={togglePasswordConfirmVisibility}
+        style={{
+          position: 'absolute',
+          right: '10px',
+          top: '88%',
+          transform: 'translateY(-50%)',
+          cursor: 'pointer',
+        }}
+      />
+      {error.passwordConfirm && (
+        <TextErrorParagraph>{error.passwordConfirm}</TextErrorParagraph>
+      )}
+    </StyledInputBox>
+  );
+};
+
+export default React.memo(SignUpPwd);
