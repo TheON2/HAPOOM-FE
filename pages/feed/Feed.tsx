6b5f4c7c81a4a93d9609efd8cf34c2eecf1dd220
SAHA-YJB <pkomom123@naver.com>
2023-08-23 20:11:10 +0900

    feat: SSG render ready

--- pages/feed/Feed.tsx
+++ pages/feed/Feed.tsx
@@ -4,24 +4,8 @@ import { useDispatch } from 'react-redux';
 import { useQuery } from 'react-query';
 import { getAuthToken } from '@/api/user';
 import { AUTH_USER, UserResponse } from '@/redux/reducers/userSlice';
-import axios from 'axios';
-import { GetServerSidePropsResult, NextPage } from 'next';
 
-export type FeedData = {
-  email: string;
-  image: string;
-  musicTitle: string;
-  musicUrl: string | null;
-  nickname: string;
-  postId: number;
-  preset: number;
-  updatedAt: string;
-  userImage: string;
-}[];
-export type FeedProps = {
-  initialFeedData: FeedData;
-};
-const Feed: NextPage<FeedProps> = ({ initialFeedData }) => {
+const Feed = () => {
   const dispatch = useDispatch();
   const isClientSide = typeof window !== 'undefined';
   const tokenExists = isClientSide ? !!localStorage.getItem('token') : false;
@@ -36,21 +20,7 @@ const Feed: NextPage<FeedProps> = ({ initialFeedData }) => {
       cacheTime: 0,
     }
   );
-  return <FeedUi initialFeedData={initialFeedData} />;
+  return <FeedUi />;
 };
 
 export default Feed;
-
-export async function getServerSideProps(): Promise<
-  GetServerSidePropsResult<{ initialFeedData: FeedData }>
-> {
-  const response = await axios.get(
-    `${process.env.NEXT_PUBLIC_LOCAL_SERVER}/api/main/feed`
-  );
-
-  return {
-    props: {
-      initialFeedData: response.data,
-    },
-  };
-}
