8f751d0cde46199c8a0182513e482a9c714df966
SAHA-YJB <pkomom123@naver.com>
2023-08-23 11:05:05 +0900

    feat: feed page UI work

--- components/Feed/FeedUi.tsx
+++ components/Feed/FeedUi.tsx
@@ -6,50 +6,84 @@ import {
   FeedMusicLikeBox,
   FeedSection,
   FeedTime,
+  FeedIcon,
   FeedUserNickName,
   MusicBox,
+  MainImageContainer,
+  Equalizer,
+  LikeIconContainer,
 } from '../../styles/feed';
 import { feedData } from './data';
+import { FeedPlayer } from '../common/SVG';
+import HeartIcon from '../common/HeartIcon';
+
+const timeSince = (date: string) => {
+  const now: Date = new Date();
+  const inputDate: Date = new Date(date);
+  const seconds: number = Math.floor(
+    (now.getTime() - inputDate.getTime()) / 1000
+  );
+  const minutes = Math.floor(seconds / 60);
+  const hours = Math.floor(minutes / 60);
+  const days = Math.floor(hours / 24);
+
+  if (days > 0) {
+    return days + '일 전';
+  } else if (hours > 0) {
+    return hours + '시간 전';
+  } else if (minutes > 0) {
+    return minutes + '분 전';
+  } else {
+    return '방금 전';
+  }
+};
 
 const FeedUi = () => {
   const data = feedData;
+
   return (
     <FeedSection>
-      <FeedContainer>
-        <FeedHeader>
-          <div>
-            <Image
-              src={data[0].userImage}
-              alt={data[0].alt}
-              width={33}
-              height={33}
-              quality={100}
-            />
-          </div>
-          <FeedUserNickName>{data[0].userNickname}</FeedUserNickName>
-          <FeedTime>{data[0].time}</FeedTime>
-          <div>...아이콘</div>
-        </FeedHeader>
+      {data.map((feed) => {
+        return (
+          <FeedContainer key={feed.id}>
+            <FeedHeader>
+              <div>
+                <Image
+                  src={feed.userImage}
+                  alt={feed.alt}
+                  width={33}
+                  height={33}
+                  quality={100}
+                />
+              </div>
+              <FeedUserNickName>{feed.userNickname}</FeedUserNickName>
+              <FeedTime>{timeSince(feed.createdAt)}</FeedTime>
+              <FeedIcon>...I</FeedIcon>
+            </FeedHeader>
 
-        <div>
-          <Image
-            src={data[0].src}
-            alt={data[0].alt}
-            width={272}
-            height={189}
-            quality={100}
-          />
-        </div>
+            <MainImageContainer>
+              <Image
+                src={feed.src}
+                alt={feed.alt}
+                width={272}
+                height={189}
+                quality={100}
+              />
+            </MainImageContainer>
 
-        <FeedMusicLikeBox>
-          <MusicBox>
-            <div>뮤직아이콘</div>
-            <div>버즈-가시</div>
-            <div>이퀄라이저</div>
-          </MusicBox>
-          <div>아이콘</div>
-        </FeedMusicLikeBox>
-      </FeedContainer>
+            <FeedMusicLikeBox>
+              <MusicBox>
+                <FeedPlayer />
+                <div>{feed.musicTitle}</div>
+                <Equalizer>EI</Equalizer>
+              </MusicBox>
+              <LikeIconContainer>
+                <HeartIcon postId={feed.id} />
+              </LikeIconContainer>
+            </FeedMusicLikeBox>
+          </FeedContainer>
+        );
+      })}
     </FeedSection>
   );
 };
