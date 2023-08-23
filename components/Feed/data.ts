50173e25458ddf857c087815eca7af58a219d7e5
SAHA-YJB <pkomom123@naver.com>
2023-08-23 11:05:05 +0900

    feat: feed page UI work

--- components/Feed/data.ts
+++ components/Feed/data.ts
@@ -4,9 +4,8 @@ export type FeedData = {
   userNickname: string;
   src: string;
   alt: string;
-  musicSinger: string;
   musicTitle: string;
-  time: string;
+  createdAt: string;
 };
 
 export const feedData: FeedData[] = 
@@ -17,8 +16,88 @@ export const feedData: FeedData[] =
     userNickname: '닉네임이다',
     src: '/c5.jpeg',
     alt: '이미지데이터',
-    musicSinger: '버즈',
-    musicTitle: '가시',
-    time: '7시에용'
-  }
+    musicTitle: '버즈-가시', 
+    createdAt: '2023-08-22 19:45:22'
+  },
+  {
+    id: 2,
+    userImage: '/b1.png',
+    userNickname: '닉네임이다',
+    src: '/c5.jpeg',
+    alt: '이미지데이터',
+    musicTitle: '버즈-가시', 
+    createdAt: '2023-08-21 19:45:22'
+  },
+  {
+    id: 3,
+    userImage: '/b1.png',
+    userNickname: '닉네임이다',
+    src: '/c5.jpeg',
+    alt: '이미지데이터',
+    musicTitle: '버즈-가시', 
+    createdAt: '2023-08-20 19:45:22'
+  },
+  {
+    id: 4,
+    userImage: '/b1.png',
+    userNickname: '닉네임이다',
+    src: '/c5.jpeg',
+    alt: '이미지데이터',
+    musicTitle: '버즈-가시', 
+    createdAt: '2023-08-19 19:45:22'
+  },
+  {
+    id: 5,
+    userImage: '/b1.png',
+    userNickname: '닉네임이다',
+    src: '/c5.jpeg',
+    alt: '이미지데이터',
+    musicTitle: '버즈-가시', 
+    createdAt: '2023-08-18 19:45:22'
+  },
+  {
+    id: 6,
+    userImage: '/b1.png',
+    userNickname: '닉네임이다',
+    src: '/c5.jpeg',
+    alt: '이미지데이터',
+    musicTitle: '버즈-가시', 
+    createdAt: '2023-08-17 19:45:22'
+  },
+  {
+    id: 7,
+    userImage: '/b1.png',
+    userNickname: '닉네임이다',
+    src: '/c5.jpeg',
+    alt: '이미지데이터',
+    musicTitle: '버즈-가시', 
+    createdAt: '2023-08-16 19:45:22'
+  },
+  {
+    id: 8,
+    userImage: '/b1.png',
+    userNickname: '닉네임이다',
+    src: '/c5.jpeg',
+    alt: '이미지데이터',
+    musicTitle: '버즈-가시', 
+    createdAt: '2023-08-15 19:45:22'
+  },
+  {
+    id: 9,
+    userImage: '/b1.png',
+    userNickname: '닉네임이다',
+    src: '/c5.jpeg',
+    alt: '이미지데이터',
+    musicTitle: '버즈-가시', 
+    createdAt: '2023-08-14 19:45:22'
+  },
+  {
+    id: 10,
+    userImage: '/b1.png',
+    userNickname: '닉네임이다',
+    src: '/c5.jpeg',
+    alt: '이미지데이터',
+    musicTitle: '버즈-가시', 
+    createdAt: '2023-08-13 19:45:22'
+  },
 ];
