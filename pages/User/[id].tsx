a272512fc68a71911c2584c9f90b393985ec5a51
TheON2 <sniperad@naver.com>
2023-08-25 11:16:45 +0900

    Fix : UserUi

--- pages/User/[id].tsx
+++ pages/User/[id].tsx
@@ -3,24 +3,20 @@ import UserUi from '../../components/User/UserUi';
 import { UserState } from '@/redux/reducers/userSlice';
 import { RootState } from '@/redux/config/configStore';
 import { useSelector } from 'react-redux';
+import { useEffect } from 'react'; // useEffect를 추가로 임포트해야 합니다.
+import { useAuth } from '@/hooks/useAuth';
 
 const UserId = () => {
   const router = useRouter();
-  const id = typeof router.query.id === 'string' ? router.query.id : '';
-  const { user }: { user: UserState['user'] } = useSelector(
-    (state: RootState) => state.user
-  );
+  const id = router.isReady
+    ? typeof router.query.id === 'string'
+      ? router.query.id
+      : ''
+    : '';
 
-  const loggedInEmail = user.email;
+  const { userData, tokenSuccess } = useAuth();
 
-  // useEffect(() => {
-  //   setCookie(null, 'userId', id, { path: '/' });
-  //   if (loggedInEmail !== null) {
-  //     setCookie(null, 'email', loggedInEmail, { path: '/' });
-  //   }
-  // }, [id, loggedInEmail]); // id와 loggedInEmail 값이 변경될 때마다 쿠키를 업데이트
-
-  return <UserUi userId={id} loggedInEmail={loggedInEmail} />;
+  return <UserUi userId={id} loggedInEmail={userData?.email} />;
 };
 
 export default UserId;
