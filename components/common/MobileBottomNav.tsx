f99ac534bc7a30d8f40d2f2645bfbf060457ab97
TheON2 <sniperad@naver.com>
2023-08-25 15:19:34 +0900

    Fix : My Page Route

--- components/common/MobileBottomNav.tsx
+++ components/common/MobileBottomNav.tsx
@@ -105,7 +105,7 @@ const MobileBottomNav = () => {
         })}
         <BottomNavItem>
           <IconBox
-            onClick={() => routerHandler(`/User/my`, 'my')}
+            onClick={() => routerHandler(`/User/${user.email}`, 'my')}
             className={active === 3 ? 'active' : ''}
           >
             <div className="image-box">
