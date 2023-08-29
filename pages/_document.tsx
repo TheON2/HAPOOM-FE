a4de50cc7fe73e38b782a6404e8e0984b15cce1a
TheON2 <sniperad@naver.com>
2023-08-29 10:54:55 +0900

    Fix : mypage

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -40,6 +40,7 @@ export default class MyDocument extends Document {
         <Head>
           <meta name="theme-color" content="#0084ff" />
           <meta name="google" content="notranslate"></meta>
+
           <link rel="manifest" href="/manifest.json" />
         </Head>
         <body>
