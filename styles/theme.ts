2f2551189a8379aeef380dd668a7d2751d4eb328
Haegnim <haegnim@gmail.com>
2023-08-30 13:46:24 +0900

    Feat : theme

--- styles/theme.ts
+++ styles/theme.ts
@@ -9,10 +9,11 @@ declare module 'styled-components' {
 }
 
 const darkModeStyles = css`
-  background-color: #fff;
-  color: #334765;
+  background-color: #132b4f;
+  color: #fff;
   /* common color start */
-  --bg-color: #fff;
+  --color: #fff;
+  --bg-color: #1b3863;
   --primary-color: #2790f6;
   --primary-opcity-color: #2790f670;
   --primary-second-color: #0084ff;
@@ -21,30 +22,30 @@ const darkModeStyles = css`
 
   --heart-color: #538cc080;
   --heart-color-hover: #0084ff90;
-  --toggle-color: #959595;
-  --nav-icon-color: #777;
+  --toggle-color: #a1afc6;
+  --nav-icon-color: #8995a7;
   --button-hover-color: #7dc1ff;
-  --button-enabled-color: #b3b3b3;
+  --button-enabled-color: #5a6a81;
   --button-text-color: #fff;
   --button-second-color: #e1e1e1;
   --button-second-hover-color: #f1f1f1;
 
   /* common color end */
   /* header color start  */
-  --header-bg-color: rgba(255, 255, 255, 0.8);
-  --header-border: 1px solid #fff;
+  --header-bg-color: #1b569580;
+  --header-border: 1px solid #1b5695;
   /* header color end  */
   /* feed color start */
-  --text-color: #232323;
+  --text-color: #fdfdfd;
   --text-time-color: #9e9e9e;
-  --feed-border: 2px solid #efefef;
+  --feed-border: 2px solid #334765;
   /* feed color end */
   /* trend color start */
   --random-picture-bg-color: #fff;
   --shadow-color: 0 16px 60px #a8b5c890;
   --text-hightligth-color: #777;
   --bar-color: #93999f;
-  --hashtag-border-color: #deefff90;
+  --hashtag-border-color: #7dc1ff;
   --hashtag-gray: #93999f;
   --hashtag-active: linear-gradient(#fff, #fff),
     linear-gradient(
@@ -55,26 +56,34 @@ const darkModeStyles = css`
   --section-gray: #f0f1f2;
   /* trend color end */
   /* write color start */
-  --text-blue-color: #1f6cb9;
-  --blue-bg-color: #eff7ff;
-  --blue-border: 2px solid #dfefff;
-  --input-bg-color: #f0efef;
-  --input-border: 2px solid #e8e8e8;
+  --text-blue-color: #174172;
+  --blue-bg-color: #eff7ff90;
+  --blue-border: 2px solid #1b5695;
+  --input-bg-color: #5a6a81;
+  --input-border: 2px solid #1b5695;
   /* write color end */
   /* search color start */
-  --search-bg-color: #fff /* search color end */;
+  --search-bg-color: #1b5695 /* search color end */;
   /* user color start */
   --text-none-active: #83818c;
   /* setting color start */
   --text-tab-gray: #8995a7;
   /* login color start */
   --text-gray-second: #b1b1b1;
+  input,
+  textarea {
+    &::placeholder {
+      color: #fff;
+    }
+    color: #fff;
+  }
 `;
 
 const lightModeStyles = css`
   background-color: #fff;
   color: #334765;
   /* common color start */
+  --color: #334765;
   --bg-color: #fff;
   --primary-color: #2790f6;
   --primary-opcity-color: #2790f670;
@@ -132,6 +141,13 @@ const lightModeStyles = css`
   --text-tab-gray: #8995a7;
   /* login color start */
   --text-gray-second: #b1b1b1;
+  input,
+  textarea {
+    &::placeholder {
+      color: #929aa7;
+    }
+    color: #334765;
+  }
 `;
 
 export const ThemeGlobalStyle = createGlobalStyle`
