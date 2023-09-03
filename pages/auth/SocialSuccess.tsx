56b2c6aafaf479a2b02850f4577096c05733aa31
TheON2 <sniperad@naver.com>
2023-09-03 19:07:57 +0900

    Fix : Social

--- pages/auth/SocialSuccess.tsx
+++ pages/auth/SocialSuccess.tsx
@@ -6,8 +6,21 @@ import { useQuery } from 'react-query';
 import { useDispatch } from 'react-redux';
 
 const SocialSuccess = () => {
+  const queryString = window.location.search;
+  const urlParams = new URLSearchParams(queryString);
+  const tempId = urlParams.get('tempId');
   const dispatch = useDispatch();
   const router = useRouter();
+
+  if (tempId) {
+    fetch(`${process.env.NEXT_PUBLIC_LOCAL_SERVER}/get-token?tempId=${tempId}`)
+      .then((res) => res.json())
+      .then((data) => {
+        const token = data.token;
+        localStorage.setItem('token', token);
+      });
+  }
+
   const { data: userData, isSuccess: tokenSuccess } = useQuery(
     'user',
     getAuthToken,
