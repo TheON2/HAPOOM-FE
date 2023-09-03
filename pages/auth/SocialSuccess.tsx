6b81febe430540af80982d99a40cdb1fe3bf44f1
TheON2 <sniperad@naver.com>
2023-09-03 19:07:57 +0900

    Fix : Social

--- pages/auth/SocialSuccess.tsx
+++ pages/auth/SocialSuccess.tsx
@@ -1,155 +1,38 @@
-import React, { useEffect, useRef, useState } from 'react';
-import MainBannerSlider from '@/components/Home/InfiniteCarousel';
-import HashtagNavBar from '@/components/Home/HashtagNavBar';
-import HashtagContents from '@/components/Home/HashtagContents';
-// import Main from '@/components/Home/HomeMain';
-import MainBanner from '@/components/Home/MainBanner';
-import Header from '@/components/common/Header';
-import PopularContentsCarousel from '@/components/Home/PopularContentsCarousel';
-import { sliderImages } from '../public/data';
-import { GetStaticProps, NextPage, GetServerSideProps } from 'next';
-import { useQuery, useQueryClient } from 'react-query';
-import { getMain, getSearch } from '@/api/post';
-import axios from 'axios';
 import { getAuthToken } from '@/api/user';
 import { AUTH_USER, UserResponse } from '@/redux/reducers/userSlice';
+import { useRouter } from 'next/router';
+import React, { useEffect } from 'react';
+import { useQuery } from 'react-query';
 import { useDispatch } from 'react-redux';
-import { setCookie } from 'nookies';
-import { HomePageLayout, MainLayout, TrendGlobalStyle } from '@/styles/home';
-import { MainPageProps } from '@/types/home';
-import StartPage from './startpage/StartPage';
 
-const Home: NextPage<MainPageProps> = ({
-  data,
-  hashtagData,
-  hashContent,
-  popularContent,
-  randomPosts,
-}) => {
+const SocialSuccess = () => {
   const dispatch = useDispatch();
-  const isClientSide = typeof window !== 'undefined';
-  const tokenExists = isClientSide ? !!localStorage.getItem('token') : false;
-  const { data: userData, isSuccess: tokenSuccess } = useQuery(
-    'user',
-    getAuthToken,
-    {
-      onSuccess: (userData: UserResponse) => {
-        dispatch(AUTH_USER(userData));
-      },
-      enabled: tokenExists,
-      cacheTime: 0,
-    }
-  );
-  const tagFilter = hashtagData.filter((tag) => tag.tag !== undefined);
-  const undefindeTag = hashtagData.filter((tag) => tag.tag === undefined);
-  const undefindeTagThumbnail =
-    undefindeTag.length !== 0 ? undefindeTag[0]?.image : '/c1.jpeg';
-  const allTagThumbnail = hashContent[0].image;
-  const [hashTag, setHashTag] = useState<string>(tagFilter[0].tag);
-  const [tagCategory, setTagCategory] = useState<string>('모든태그');
-  const [isClick, setIsClick] = useState<boolean>(false);
-  const mainRef = useRef<HTMLDivElement | null>(null);
-  const onClickBottomNavHandler = () => {
-    setIsClick(!isClick);
-  };
-  const { data: hashtagSearch, isLoading } = useQuery(
-    ['hashtag', hashTag],
-    () => getSearch({ search: hashTag, option: 'tags' }),
-    {
-      enabled: tagCategory === 'unique',
-      staleTime: 60 * 1000,
-    }
-  );
-  const HashTagScrollTopHandler = () => {
-    if (isClick && mainRef.current) {
-      mainRef.current.scrollTop = 0;
-    }
-  };
-  const handleWheel = (event: WheelEvent) => {
-    if (event.deltaY > 0) {
-      return setIsClick(true);
-    }
-    if (event.deltaX !== 0) {
-      return;
-    }
-    if (!mainRef.current) return;
-    if (mainRef.current?.scrollTop <= 0 && event.deltaY <= 0) {
-      return setIsClick(false);
-    }
-  };
-
+  const router = useRouter();
   useEffect(() => {
-    window.addEventListener('wheel', handleWheel);
-    return () => {
-      window.removeEventListener('wheel', handleWheel);
-    };
-  }, [isClick]);
+    // 쿼리에서 token 값을 가져옵니다.
+    const { token } = router.query;
 
-  if (typeof window !== 'undefined') {
-    setCookie(null, 'update', '1', { path: '/' });
-    setCookie(null, 'updateId', '0', { path: '/' });
-  }
+    if (token) {
+      // 로컬 스토리지에 토큰을 저장합니다.
+      window.localStorage.setItem('token', token as string);
+      router.push('/');
+      // 이후 다른 라우트로 이동하거나 추가 작업을 수행할 수 있습니다.
+      // 예: router.push('/');
+    }
+  }, [router.query, router]);
 
-  return (
-    <>
-      <StartPage />
-      <TrendGlobalStyle />
-      <Header className={'trend'} $sticky={!isClick} />
-      <HomePageLayout>
-        <MainBanner
-          data={data}
-          $isClick={isClick}
-          randomPosts={randomPosts}
-          onClickBottomNavHandler={onClickBottomNavHandler}
-        />
-        <HashtagNavBar
-          data={tagFilter}
-          $isClick={isClick}
-          setIsClick={setIsClick}
-          onClickEvent={onClickBottomNavHandler}
-          HashTagScrollTopHandler={HashTagScrollTopHandler}
-          hashTag={hashTag}
-          setHashTag={setHashTag}
-          setTagCategory={setTagCategory}
-          undefindeTagThumbnail={undefindeTagThumbnail}
-          allTagThumbnail={allTagThumbnail}
-        />
-        <MainLayout ref={mainRef}>
-          <HashtagContents
-            serverPropData={hashContent}
-            tagData={hashtagSearch}
-            undefindeTag={undefindeTag}
-            hashTag={hashTag}
-            tagCategory={tagCategory}
-          />
-          <PopularContentsCarousel data={popularContent} />
-        </MainLayout>
-      </HomePageLayout>
-    </>
-  );
+  // const { data: userData, isSuccess: tokenSuccess } = useQuery(
+  //   'user',
+  //   getAuthToken,
+  //   {
+  //     onSuccess: (userData: UserResponse) => {
+  //       dispatch(AUTH_USER(userData));
+  //       router.push('/');
+  //     },
+  //     cacheTime: 0,
+  //   }
+  // );
+  return <></>;
 };
 
-export default Home;
-
-export const getStaticProps: GetStaticProps = async () => {
-  const response = await axios.get(
-    `${process.env.NEXT_PUBLIC_LOCAL_SERVER}/api/main`
-  );
-
-  const data = sliderImages;
-  // const hashtagData = hashtagImages;
-
-  return {
-    props: {
-      data,
-      hashtagData: response.data.mainTags,
-      hashContent: response.data.posts,
-      popularContent: response.data.likePosts,
-    },
-    revalidate: 60,
-    //MEMO : revalidate
-    //stale : 신선하지 않은 데이터
-    //5초 동안 hit가 유지된다.
-    //데이터가 변하지 않으면 pre-render가 실행되지 않는다.
-  };
-};
+export default SocialSuccess;
