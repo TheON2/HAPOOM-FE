81717136bb4257e7a33499aa075792ea4916a034
doyoung1002 <leedoyoung.oc@gmail.com>
2023-09-04 20:15:06 +0900

    fix

--- components/User/UserProfileCard.tsx
+++ components/User/UserProfileCard.tsx
@@ -1,34 +1,77 @@
 import React from 'react';
 import {
-  LikePostSuggestionBox,
-  ImageBox,
-  InfoContainer,
-  InfoText,
-  InfoNumber,
+  FollowBox,
+  NicknameBox,
+  ProfileContentsBox,
+  UserProfileCardBox,
 } from '@/styles/user';
+import b1 from '../../public/b1.png';
 import { UserPageData } from './UserUi';
-
-interface postsCountProps {
+import Link from 'next/link';
+import { UserState } from '@/redux/reducers/userSlice';
+import { useSelector } from 'react-redux';
+import { RootState } from '@/redux/config/configStore';
+import ProfileImage from '../common/ProfileImage';
+interface UserProfileCardProps {
   data: UserPageData | undefined;
   userId: string;
 }
 
-const UserLikePostSuggestion: React.FC<postsCountProps> = ({ data }) => {
+interface FollowLinkProps {
+  type: 'followers' | 'followings';
+  count: number;
+  userId: number | null;
+}
+
+const FollowLink: React.FC<FollowLinkProps> = ({ type, count, userId }) => (
+  <Link href={`/User/FollowTab?tab=${type}&userId=${userId}`}>
+    {type === 'followers' ? '팔로워' : '팔로잉'} {count}
+  </Link>
+);
+
+const UserProfileCard: React.FC<UserProfileCardProps> = ({ data, userId }) => {
+  const { user }: { user: UserState['user'] } = useSelector(
+    (state: RootState) => state.user
+  );
+  if (!data) {
+    return null;
+  }
+
+  const followerCount = data.followerCount || 0;
+  const followingCount = data.followingCount || 0;
+
+  const userImageSrc = data.user.userImage?.startsWith('http')
+    ? data.user.userImage
+    : b1;
+
   return (
-    <LikePostSuggestionBox>
-      <ImageBox>
-        <InfoContainer>
-          <InfoText>게시물</InfoText>
-          <InfoNumber>{data?.postsCount}</InfoNumber>
-        </InfoContainer>
-      </ImageBox>
-      <ImageBox>
-        <InfoContainer>
-          <InfoText>좋아요</InfoText>
-          <InfoNumber>{data?.likePostsCount}</InfoNumber>
-        </InfoContainer>
-      </ImageBox>
-    </LikePostSuggestionBox>
+    <UserProfileCardBox>
+      <div className="img">
+        <ProfileImage
+          userImage={data?.user?.userImage}
+          preset={data?.user?.preset}
+        />
+      </div>
+      <ProfileContentsBox>
+        <NicknameBox>
+          <p className="nickName">{data.user.nickname}</p>
+        </NicknameBox>
+        <FollowBox>
+          <FollowLink
+            type="followers"
+            count={followerCount}
+            userId={data.user.userId}
+          />
+          <span>|</span>
+          <FollowLink
+            type="followings"
+            count={followingCount}
+            userId={data.user.userId}
+          />
+        </FollowBox>
+      </ProfileContentsBox>
+    </UserProfileCardBox>
   );
 };
-export default UserLikePostSuggestion;
+
+export default UserProfileCard;
