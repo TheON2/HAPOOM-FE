cc242588872c3eda107fe1e11db30a1419c15028
Haegnim <haegnim@gmail.com>
2023-09-06 10:37:04 +0900

    Fix : splash page

--- components/common/MoveStartpage.tsx
+++ components/common/MoveStartpage.tsx
@@ -2,8 +2,6 @@ import React, { useRef } from 'react';
 import styled from 'styled-components';
 import { StartPageCloud } from './SVG';
 import { identity } from 'lodash';
-import { useRouter } from 'next/router';
-
 const HeadText = styled.h1`
   color: #2790f6;
   font-size: 40px;
@@ -45,11 +43,9 @@ const StartPageSection = styled.section`
 
 const MoveStartpage = () => {
   const noneStartPage = useRef<any>();
-  const route = useRouter();
   React.useEffect(() => {
     const timer = setTimeout(() => {
       noneStartPage.current.style.display = 'none';
-      // route.push('/');
     }, 3500);
 
     return () => clearTimeout(timer);
